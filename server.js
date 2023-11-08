9af37869de56b4e7e50467544677c164a04dc82b
Nirav <niravpatelp129@gmail.com>
2023-11-08 14:39:47 -0500

    * fix(server.js): remove unnecessary options object from mongoose.connect() call

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const app = express();
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_CONNECTION_STRING, {})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB', err));
 
