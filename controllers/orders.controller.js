7d973429d8aa16dc67f4016248aca5ec6edbd209
Nirav <niravpatelp129@gmail.com>
2023-12-11 22:39:01 -0500

    * feat(orders.controller.js): fetch pending orders and their associated products from Shopify API

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 
 const fetchPendingOrders = async (req, res) => {
+  const productsIds = new Set();
+
   axios
     .get(
       'https://icy-toronto.myshopify.com/admin/api/2023-04/orders.json?status=open&created_at_min=2023-11-11',
@@ -10,7 +12,36 @@ const fetchPendingOrders = async (req, res) => {
         },
       },
     )
-    .then((response) => {
+    .then(async (response) => {
+      response.data?.orders.forEach((order) => {
+        order.line_items.forEach((item) => {
+          if (!item.product_id) return;
+
+          productsIds.add(item.product_id);
+        });
+      });
+
+      const products = await axios.get(
+        `https://icy-toronto.myshopify.com/admin/api/2023-04/products.json?ids=${Array.from(
+          productsIds,
+        ).join(',')}}`,
+        {
+          headers: {
+            'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
+          },
+        },
+      );
+
+      response.data?.orders.forEach((order) => {
+        order.line_items.forEach((item) => {
+          if (!item.product_id) return;
+
+          const product = products.data.products.find((product) => product.id === item.product_id);
+
+          item.product = product;
+        });
+      });
+
       res.send(response.data?.orders);
     })
     .catch((error) => {
